592d846d580cc07afa3e255579717dd750e12a10
Anuj Gupta <anujiitm2004@gmail.com>
2025-05-26 20:26:28 +0530

    first commit

---
+++ main_1.py
@@ -0,0 +1,25 @@
+import langchain
+import openai
+from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+# from langchain_openai import OpenAIEmbeddings
+# import os
+# import streamlit as st
+
+
+
+# print(langchain.__version__)
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# embedding = OpenAIEmbeddings(model='text-embedding-3-small', dimensions=32)
+# documents = [
+#     "Delhi is the capital of India"
+# ]
+#
+# result = embedding.embed_documents(documents)
+# print(result)
+
+# model = ChatOpenAI(model='gpt-4o-mini-2024-07-18', temperature=1.5, max_completion_tokens=10)
+# result = model.invoke("Write a 10 line poem on covid")
+# print(result.content)
